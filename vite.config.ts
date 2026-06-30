cd04089481ca474d084d3b56b1887e7c9644657a
ridwanmridwan881-web <ridwanmridwan881@gmail.com>
2026-06-30 16:12:38 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,1 +1,10 @@
-400: Invalid request
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+});
